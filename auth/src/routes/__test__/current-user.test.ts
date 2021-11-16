2f465ea0d3b3ef82059aa930f0888d10ad6f6afa
RedFour <RedFour@users.noreply.github.com>
2021-11-15 16:20:06 -0800

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -13,7 +13,7 @@ it('responds with details about the current user', async () => {
     .get(CURRENT_USER_API)
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(res.body.currentUser.email).toEqual(EMAIL_VALID);
 });
